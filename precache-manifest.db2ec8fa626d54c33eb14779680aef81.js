00623c39b79b1eca880bf78a30b05ed6dc517aba
Sergey Sydorenko <sidose4@gmail.com>
2021-03-02 22:44:23 +0200

    Updates

---
+++ precache-manifest.db2ec8fa626d54c33eb14779680aef81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195acab6b26a3b163c3607e79a2f62d3",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "d6b0ac05893f74e5c756",
+    "url": "/Tic-Tac-Toe/static/css/main.42c2e523.chunk.css"
+  },
+  {
+    "revision": "91f54fda10cc62b55d35",
+    "url": "/Tic-Tac-Toe/static/js/2.25f86fd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tic-Tac-Toe/static/js/2.25f86fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b0ac05893f74e5c756",
+    "url": "/Tic-Tac-Toe/static/js/main.899d99b8.chunk.js"
+  },
+  {
+    "revision": "5965f36d7f72120b2bad",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.49965c0b.js"
+  }
+]);
